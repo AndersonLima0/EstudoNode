4621d1059968042beb0221e78dce611212388ced
AndersonLima0 <84778277+AndersonLima0@users.noreply.github.com>
2023-01-18 01:17:16 -0300

    Testando o endpoint de criação de usuários com o Postman

--- modules/express.js
+++ modules/express.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const UserModel = require("../src/models/user.model");
 
 const app = express();
+
+app.use(express.json());
+
 app.get("/home", (req, res) => {
   res.contentType("application/html");
   res.status(200).send("<h1>Hello world</h1>");
@@ -19,6 +23,17 @@ app.get("/users", (req, res) => {
   ];
 
   res.status(200).json(users);
+});
+
+//cria usuario no postman
+app.post("/users", async (req, res) => {
+  try {
+    const user = await UserModel.create(req.body);
+
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
 });
 
 const port = 8080;
