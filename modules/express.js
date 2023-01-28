ccc0556b9654cf35f446aa0f5708b0e1439c4d2b
AndersonLima0 <84778277+AndersonLima0@users.noreply.github.com>
2023-01-28 01:20:42 -0300

    Criando endpoint de listagem de usuarios

--- modules/express.js
+++ modules/express.js
@@ -35,6 +35,15 @@ app.post("/users", async (req, res) => {
     res.status(500).send(error.message);
   }
 });
+//Listagem de usuarios
+app.get("/users", async (req, res) => {
+  try {
+    const users = await UserModel.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
 
 const port = 8080;
 app.listen(port, () => console.log(`Rodando com express na porta ${port}`));
