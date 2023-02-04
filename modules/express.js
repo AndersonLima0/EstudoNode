5b56db0feafa05f92068cc92b4cc95c3b1201c71
AndersonLima0 <84778277+AndersonLima0@users.noreply.github.com>
2023-02-04 16:20:57 -0300

    Endpoint para deleção de usuario

--- modules/express.js
+++ modules/express.js
@@ -66,6 +66,16 @@ app.patch("/users/:id", async (req, res) => {
     res.status(500).send(error.message);
   }
 });
+//deletando usuario por id
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await UserModel.findByIdAndRemove(id);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
 
 const port = 8080;
 app.listen(port, () => console.log(`Rodando com express na porta ${port}`));
