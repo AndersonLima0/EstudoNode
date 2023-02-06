9ef931b6565b2f1aad7061c97ad9d467d7a9bb33
AndersonLima0 <84778277+AndersonLima0@users.noreply.github.com>
2023-02-06 15:48:09 -0300

    Middlewares

--- modules/express.js
+++ modules/express.js
@@ -5,6 +5,15 @@ const app = express();
 
 app.use(express.json());
 
+//middleware são executados antes de qualquer requisição
+app.use((req, res, next) =>{
+  console.log(`Request Type: ${req.method}`);
+  console.log(`Content Type: ${req.headers["content-type"]}`)
+  console.log(`Date: ${new Date()}`)
+
+  next();
+})
+
 app.get("/home", (req, res) => {
   res.contentType("application/html");
   res.status(200).send("<h1>Hello world</h1>");
